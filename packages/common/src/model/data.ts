cca777299ac56091d1b52a054d00ca3a5e60c409
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2024-07-11 22:01:48 +0200

    change description a little bit

--- packages/common/src/model/data.ts
+++ packages/common/src/model/data.ts
@@ -9,6 +9,8 @@ import {
 
 export const name: string = "Aliaksandr Skuratovich";
 
+const withDot = (str: string) => (str.endsWith(".") ? str : str + ".");
+
 export const contactInfoData: ContactInfoData = {
   LinkedIn: "https://www.linkedin.com/in/aliaksandr-skuratovich-4a2ab01a0/",
   GitHub: "https://github.com/SkuratovichA",
@@ -19,43 +21,44 @@ export const contactInfoData: ContactInfoData = {
 export const workExperienceData: WorkExperienceData[] = [
   {
     position: "Senior Software Engineer",
-    company: "Gen",
+    company: "Gen (ex Avast)",
     type: "Full-time",
     location: "Prague, Czechia",
-    duration: "May 2024 - Present",
+    duration: "2024 - Present",
     responsibilities: [
-      "Doing internal projects for developers, product and project managers leveraging Spotify Backstage frameworks",
-      "Handling various aspects of the project lifecycle, including CI/CD, process establishment (Jira, Confluence), conflict resolution within the team, and communication with end users/customers and product shaping",
-      "Skills: Team leading, TeamCity, React, TypeScript, Kubernetes, Docker, etc",
-    ],
+      "Working on an internal projects for developers, product and project managers leveraging Spotify Backstage frameworks",
+      "Developing AI-powvered features to enhace the developer/user experience",
+      "Handling various aspects of a project lifecycle, including CI/CD, process establishment (Jira, Confluence), conflict resolution within the team, communication with end users/customers, and other possible stuff",
+      "Skills: Team leading, TeamCity, GCP, React, Kubernetes, Docker, and many more",
+    ].map(withDot),
   },
   {
     position: "Chief Technology Officer",
     company: "TAP",
     location: "Planet Earth",
     type: "Part-time",
-    duration: "May 2024 - Present",
+    duration: "2024 - Present",
     responsibilities: [
       "We are developing an ad aggregator platform that connects customers with businesses (telegram public channels)",
-      "We use AI to analyze their target audience. They both can then match each other based on their target audience",
+      "We use AI to analyze their target audience and find the best matches for both businesses and platforms",
       "Additionally, the platform serves as an escrow agent, ensuring secure and trustworthy transactions with smart contracts",
-      "Skills: Team leading, Management, Conflict resolution, Communication, Hiring, React/Remix, GraphQL, Node, GCP, you name it",
-    ],
+      "Skills: System Architecture, Team leading, Management, Hiring, React/Remix, GraphQL, NodeJS, GCP, you name it",
+    ].map(withDot),
   },
   {
-    position: "Full Stack Engineer",
+    position: "Software Engineer",
     company: "Salsita Software",
     type: "Full-time",
     location: "Prague, Czechia · Hybrid",
-    duration: "February 2023 - May 2024",
+    duration: "2023 - 2024",
     responsibilities: [
-      "Successfully developed an LLM-powered UI for a 3D product configurator from inception to MVP",
+      "Successfully developed an LLM-powered UI for a 3D product configurator from inception and PoC to MVP",
       "Enhanced CI/CD pipelines and streamlined development processes for eBay’s Social Hub platform",
-      "Key Skills: NodeJS (Koa), OpenAI API, TypeScript/React, RxJS, Emotion React, Framer Motion, Kotlin (Spring Boot), Microservices, GraphQL, Jenkins",
-    ],
+      "Skills: OpenAI API, NodeJS, React, GraphQL, Jenkins",
+    ].map(withDot),
   },
   {
-    position: "Junior Researcher",
+    position: "AI Researcher",
     type: "Full-time",
     company: "BUT Speech",
     location: "Brno, South Moravia, Czechia · On-site",
@@ -64,7 +67,8 @@ export const workExperienceData: WorkExperienceData[] = [
       "Training and fine-tuning models for ASR and NLP tasks",
       "Researching state-of-the-art streamable ASR architectures",
       "Using and contributing to frameworks such as SpeechBrain, Nvidia NeMo, and HuggingFace",
-    ],
+      "Skills: Academic Research, Python, Pytorch, HuggingFace, SpeechBrain, Nvidia NeMo, ASR, NLP",
+    ].map(withDot),
   },
   {
     position: "Chief Executive Officer",
@@ -76,7 +80,7 @@ export const workExperienceData: WorkExperienceData[] = [
       "Developing various projects, including a GPT-4 powered CV builder, a Telegram bot for managing conversations, and a GPT-powered event manager in Telegram",
       "Spending time on Neovim configuration and other nerdy tasks such as tmux",
       "Creating a multi-platform AI-powered application for managing debts and an ad aggregator platform using AI to analyze target audiences and ensure secure transactions with smart contracts",
-    ],
+    ].map(withDot),
   },
 ];
 
@@ -84,25 +88,24 @@ export const projectsData: ProjectData[] = [
   {
     name: "AI 3D Product Configurator",
     description: [
-      "Developed an innovative 3D product configurator using GPT-4 API for prompt engineering and UI/UX design.",
-      "Skills: TypeScript, Node.js, React, OpenAI API, Prompt Engineering, Koa.js, UI/UX",
-    ].join("</br>"),
+      "Developed an innovative 3D product configurator using GPT-4 API for prompt engineering and UI/UX design",
+      "Skills: TypeScript, NodeJS, React, OpenAI API, Prompt Engineering, UI/UX",
+    ].map(withDot).join("</br>"),
   },
   {
     name: "Various Coding Projects",
     description: [
-      "Developed a lightweight compiler for Lua in C, a diagram editor in C++/Qt, and an information system for organizing conferences using Django, React, and Postgres.",
-      "Created numerous smaller projects including a packet sniffer, spreadsheet processors, and an IoT project.",
-      "Written over 50 Python scripts for dataset preparation.",
-    ].join("</br>"),
+      "Developed a lightweight compiler for Lua in C, a diagram editor in C++/Qt, and an information system for organizing conferences using Django, React, and Postgres",
+      "Created numerous smaller projects including a packet sniffer, spreadsheet processors, and an IoT project",
+      "Written over 50 Python scripts for dataset preparation",
+    ].map(withDot).join("</br>"),
   },
   {
     name: "Machine Learning Projects",
     description: [
-      "Developed several E2E ASR systems and fine-tuned Whisper model for Albaizyn ASR challenge and MTM22 challenge.",
-      "Worked on NER using CRF and BERT, voice and image identification, and document classification using LDA.",
-      "Utilized WandB extensively and explored quantization, pruning, and other optimizations for efficient inference.",
-    ].join("</br>"),
+      "Developed several E2E ASR, fine-tuned a bunch of models, participated in Albaizyn challenge and MTM22 challenge",
+      "Utilized WandB extensively and explored quantization, pruning, and other optimizations for efficient inference",
+    ].map(withDot).join("</br>"),
   },
 ];
 
@@ -121,8 +124,8 @@ export const educationData: EducationData[] = [
   {
     institution: "Brno University of Technology",
     degree: "Bachelor in Information Technology",
-    duration: "October 2020 — Present",
-    description: "Top student with the highest grades",
+    duration: "2020 — 2023",
+    description: "Top 1% student",
     keywords: ["Python", "C/C++", "Machine Learning", "Deep Learning"],
     location: "Brno, Czechia",
   },
@@ -130,37 +133,40 @@ export const educationData: EducationData[] = [
     institution: "Unicorn University",
     degree: "Bachelor in Economics and Management",
     duration: "October 2018 — October 2020",
-    description: "Top student with the highest grades",
+    description: "Not top 1% student",
     keywords: ["Economics", "Management", "Finance"],
     location: "Prague, Czechia",
   },
 ];
 
 export const skillsData: SkillsData = {
-  "Soft Skills": ["Teamwork", "Problem Solving", "Fast Learning"],
+  "Soft Skills": [
+    "Management",
+    "Hiring",
+    "Team Leading",
+    "Problem Solving"
+  ],
   "Hard Skills": [
-    "JavaScript/TypeScript",
-    "Python",
-    "C/C++",
-    "CSS/HTML",
-    "Bash",
+    "Software Engineering",
+    "Prompt Engineering",
+    "System Architecture Design",
   ],
   Tools: [
-    "React",
-    "Node.js/Koa.js/Next.js",
-    "Pytorch",
-    "HuggingFace",
-    "Django/Flask",
-    "Postgres/MongoDB",
-    "Docker/Kubernetes",
+    "TypeScript/Python/Kotlin/Go/Rust",
+    "React/Remix/NextJS",
+    "Koa/Express/Flask/Django/NestJS",
+    "Pytorch/HuggingFace",
+    "Postgres/MongoDB/Redis/GraphQL",
+    "Docker/Kubernetes/GCP/AWS/Heroku/Azure",
+    "TeamCity/Jenkins/Github Actions"
   ],
-  Languages: ["Russian", "English", "Czech", "Belarusian"],
+  Languages: ["Russian", "English", "Czech"],
 };
 
 export const aboutMeData = [
-  "Programming since the age of 17.",
-  "I'm a software engineer with a blend of full-stack and machine learning experience.",
-  "Studied at Brno University of Technology and Unicorn University, excelling as the top student with the highest grades. Pointlessly spent 3 years of my life studying 15 hours/day.",
-  "Did some academic research at BUT Speech, mostly preparing data, pipelines, and experimenting with architectures (mostly for ASR and NER).",
-  "When not coding, I indulge in hobbies like cycling, running, swimming, learning neural biology, economics and history.",
-].join(" ");
+  "Programming since the age of -1",
+  "A goulash with a blend of software engineering, AI, and a pinch of soft skills",
+  "Have done some product-centric academic research in AI (NLP, ASR) during the university times",
+  "When not coding, I do cycling, swimming, learn history", 
+  "Also, I build custom keyboards and use neovim",
+].map(withDot);
